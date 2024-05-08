767d1a40000fdf4eed4f14bca018dd7eda3579d4
Sachin Gowda <sachingowda1619@gmail.com>
2024-05-09 01:37:34 +0530

    Portfolio animation added

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -8,22 +8,31 @@ const Portfolio = () => {
   return (
     <div className='portfolio'>
         <div className='portfolio-what'>What I Did ?</div>
-        <div className='portfolio-header'>Portfolio</div>
-        <div className='portfolio-image-container'>
-            <img className='work-image' src={portfolio_image_1} alt='My work'/>
-            <div className='image-inside-1'>
-                <h3 className='portfolio-header'>Web designing</h3>
-                <p className='portfolio-content'>Category: Web templates</p>
+        <div className='portfolio-main-header'>Portfolio</div>
+        <div className='portfolio-main-container'>
+            <div className='portfolio-container'>
+                <img className='work-image' src={portfolio_image_1} alt='My work'/>
+                <div className='image-inside'>
+                    <h3 className='portfolio-header'>Web designing</h3>
+                    <p className='portfolio-content'>Category: Web templates</p>
+                </div>
+                <div className='animation-div'></div>
             </div>
-            <img className='work-image' src={portfolio_image_2} alt='My work'/>
-            <div className='image-inside-2'>
-                <h3 className='portfolio-header'>Web designing</h3>
-                <p className='portfolio-content'>Category: Web templates</p>
+            <div className='portfolio-container'>
+                <img className='work-image' src={portfolio_image_2} alt='My work'/>
+                <div className='image-inside'>
+                    <h3 className='portfolio-header'>Web designing</h3>
+                    <p className='portfolio-content'>Category: Web templates</p>
+                </div>
+                <div className='animation-div'></div>
             </div>
-            <img className='work-image' src={portfolio_image_3} alt='My work'/>
-            <div className='image-inside-3'>
-                <h3 className='portfolio-header'>Web designing</h3>
-                <p className='portfolio-content'>Category: Web templates</p>
+            <div className='portfolio-container'>
+                <img className='work-image' src={portfolio_image_3} alt='My work'/>
+                <div className='image-inside'>
+                    <h3 className='portfolio-header'>Web designing</h3>
+                    <p className='portfolio-content'>Category: Web templates</p>
+                </div>
+                <div className='animation-div'></div>
             </div>
         </div>
     </div>
