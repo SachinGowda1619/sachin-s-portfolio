caac23cb6347aae120260bda70bd7c1dd2f7583a
Sachin Gowda <sachingowda1619@gmail.com>
2024-05-16 01:18:53 +0530

    Responsive design initial setup commit

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ const App = () => {
   return (
     <div className='app'>
       <Home />
-      <About />
+      {/* <About />
       <Portfolio />
       <Testmonial />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
