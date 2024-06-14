9833197d34739463caf86b1a934f07dfeab988b6
Sachin Gowda <sachingowda1619@gmail.com>
2024-06-14 23:05:26 +0530

    Hamburger menu and transition added for mobile view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Hamburger from 'hamburger-react';
 const Home = () => {
 
     const [hamBurgerOpen, setHamBurgerOpen] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
@@ -13,6 +14,19 @@ const Home = () => {
 
         const handleScroll = () => {
             console.log("Scrolled values => 'X': ", window.scrollX, ", 'Y': ", window.scrollY);
+
+            if(window.scrollY === 0) {
+                setScrolled(false);
+                document.querySelector(".header-container")?.classList.remove("header-container-scrolled");
+                document.querySelector(".header")?.classList.remove("header-scrolled");
+                document.querySelector(".menu-list")?.classList.remove("menu-list-scrolled");
+            }
+            else {
+                setScrolled(true);
+                document.querySelector(".header-container")?.classList.add("header-container-scrolled");
+                document.querySelector(".header")?.classList.add("header-scrolled");
+                document.querySelector(".menu-list")?.classList.add("menu-list-scrolled");
+            }
         }
         
         window.addEventListener('resize', handleResize);
@@ -24,20 +38,31 @@ const Home = () => {
         };
     }, []);
 
+    useEffect(() => {
+        if(window.scrollY === 0) {
+            document.querySelector(".menu-list")?.classList.remove("menu-list-scrolled");
+        }
+        else {
+            document.querySelector(".menu-list")?.classList.add("menu-list-scrolled");
+        }
+    }, [hamBurgerOpen]);
+
   return (
     <div className='home'>
         <div className='home-shadow'>
-            <div className='header'>
-                {hamBurgerOpen &&
-                    <ul className='menu-list'>
-                        <li className='menu-item'>Home</li>
-                        <li className='menu-item'>About</li>
-                        <li className='menu-item'>Portfolio</li>
-                        <li className='menu-item'>Testmonial</li>
-                        <li className='menu-item'>Contact</li>
-                    </ul>
-                }
-                <Hamburger toggled={hamBurgerOpen} toggle={setHamBurgerOpen} color='white'/>
+            <div className='header-container'>
+                <div className='header'>
+                    {hamBurgerOpen &&
+                        <ul className='menu-list'>
+                            <li className='menu-item'>Home</li>
+                            <li className='menu-item'>About</li>
+                            <li className='menu-item'>Portfolio</li>
+                            <li className='menu-item'>Testmonial</li>
+                            <li className='menu-item'>Contact</li>
+                        </ul>
+                    }
+                    <Hamburger toggled={hamBurgerOpen} toggle={setHamBurgerOpen} color={!scrolled ? 'white' : '#695aa6'}/>
+                </div>
             </div>
             <div className='home-container'>
                 <h2>
@@ -53,7 +78,6 @@ const Home = () => {
                     Visit My Works
                 </button>
             </div>
-            
         </div>
     </div>
   )
